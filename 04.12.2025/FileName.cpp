3be0543d2504a1c899826605d5bf7d043ed043fc
Nikita <prayde.polyakov@gmail.com>
2025-12-04 10:23:32 +0200

    Add project files.

---
+++ 04.12.2025/FileName.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct IPrint
+{
+	virtual void PrintConsole() = 0;
+};
+
+
+
+class Transport : IPrint
+{
+protected:
+	string model;
+	double price;
+public:
+	Transport() { price = 0; }
+	Transport(string m, double p)
+	{
+		model = m;
+		price = p;
+	}
+};
+
+
+
+class Auto : Transport
+{
+	string color;
+	double speed;
+public:
+	Auto() {}
+	Auto(string c, double s)
+	{
+		color = c;
+		speed = s;
+	}
+	virtual void PrintConsole()
+	{
+		cout << "Model\t" << model << "Price:\t" << price << "Color:\t" << color << "Speed:\t" << speed << endl;
+	}
+};
+
+class Plane : Transport
+{
+	int passangers;
+	int wings;
+public:
+	Plane() {}
+	Plane(int p, int w)
+	{
+		passangers = p;
+		wings = w;
+	}
+	virtual void PrintConsole()
+	{
+		cout << "Model\t" << model << "Price:\t" << price << "Passangers:\t" << passangers << "Wings:\t" << wings << endl;
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Animal : IPrint
+{
+protected:
+	string name;
+	int weight;
+public:
+	Animal() { weight = 0; }
+	Animal(string n, int w)
+	{
+		name = n;
+		weight = w;
+	}
+};
+
+
+
+
+class Pinguin : Animal
+{
+	double family;
+	int weight;
+public:
+	Pinguin() {}
+	Pinguin(double f, int w)
+	{
+		family = f;
+		weight = w;
+	}
+	virtual void PrintConsole()
+	{
+		cout << "Name\t" << name << "Weight:\t" << weight << "Family:\t" << family << "Weight:\t" << weight << endl;
+	}
+};
+
+
+class Dog : Animal
+{
+	string breed;
+	string sex;
+public:
+	Dog() {}
+	Dog(string b, string s)
+	{
+		sex = s;
+		breed = b;
+	}
+	virtual void PrintConsole()
+	{
+		cout << "Name\t" << name << "Weight:\t" << weight << "Sex:\t" << sex << "Breed:\t" << breed << endl;
+	}
+};
+
+
+
+int main()
+{
+
+}
